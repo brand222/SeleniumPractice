1eed427b136afaaf91c97ff1fa969d6f8557eadc
Brandon McDonald <bmcdonald@tmlirp.org>
2018-06-05 15:43:25 -0500

    Added log4j stuff

---
+++ src/log4jTutorial/LoggingDemo.java
@@ -0,0 +1,18 @@
+package log4jTutorial;
+//here are the log4j imports
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class LoggingDemo {
+	
+	//this instance MUST be static and final
+	//here it will tell the logger the class it will be logging from
+	private static final Logger log = LogManager.getLogger(LoggingDemo.class.getName());
+
+	public static void main(String[] args) {
+		log.debug("Debug Message Logged");
+		log.error("Error message Logged");
+		log.fatal("Fatal message Logged");
+		
+	}
+}
